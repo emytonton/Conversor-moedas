dea32a934c225892cd3750f132078672912b575b
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-06 21:12:47 -0300

    front finalizado

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import {React, useState, useEffect} from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator, TextInput, TouchableOpacity } from "react-native";
 import PickerItem from "./src/Picker";
 import { api } from "./src/services/api";
 
 
 export default function App(){
 
+  const [moedas, setMoedas] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [moedaSelecionada, setMoedaSelecionada] = useState(null)
+
   useEffect( () => {
     async function  loadMoedas() {
       const response = await api.get('all')
       let arrayMoedas = [];
-      Object.keys.apply(response.data).map((key) =>  {
+      Object.keys(response.data).map((key) =>  {
         arrayMoedas.push({
           key: key,
-          label: label,
+          label: key,
           value: key
         })
       })
+      setMoedas(arrayMoedas)
+      setMoedaSelecionada(arrayMoedas[0].key)
+      setLoading(false)
     }
 
     loadMoedas();
@@ -27,15 +34,40 @@ export default function App(){
 
 
 
-
+if(loading){
+  return(
+    <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#101215' }}>
+      <ActivityIndicator style = {{color : '#FFF', size: 'large'}}/>
+    </View>
+  )
+}else{
   return(
     <View style = {styles.container}>
       <View style = {styles.areaMoeda}>
       <Text style = {styles.titulo}>Selecione sua moeda:</Text>
-      <PickerItem/>
+      <PickerItem
+      moedas = {moedas}
+      moedaSelecionada = {moedaSelecionada}
+      onChange = {(moeda)  => setMoedaSelecionada(moeda) }
+
+      />
       </View>
+
+      <View style = {styles.areaValor}>
+      <Text style = {styles.titulo}>Digite um valor para converter em (R$) </Text>
+      <TextInput
+      placeholder="EX: 1.50"
+      style = {styles.input}
+      keyboardType="numeric"
+      />
+      </View>
+
+      <TouchableOpacity style = {styles.botaoArea}>
+        <Text style = {styles.botaoText}>Converter</Text>
+      </TouchableOpacity>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -50,7 +82,8 @@ const styles = StyleSheet.create({
     width:'90%',
     borderTopLeftRadius: 8,
     borderTopRightRadius:8,
-    padding: 8
+    padding: 8,
+    marginBottom: 1
   },
   titulo: {
     fontSize:16,
@@ -58,6 +91,33 @@ const styles = StyleSheet.create({
     fontWeight:'500',
     paddingLeft: 5,
     paddingTop:5
+  },
+  areaValor:{
+    width: '90%',
+    backgroundColor: '#F9F9F9',
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  input:{
+    width: '100%',
+    padding: 8,
+    fontSize: 18,
+    color: '#000'
+  },
+  botaoArea:{
+    width:'90%',
+    backgroundColor: '#FB5B57',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius:8
+  },
+  botaoText: {
+    color: '#000',
+    fontWeight: 'bold',
+    fontSize: 16
+
   }
 })
 
--- src/Picker/index.js
+++ src/Picker/index.js
@@ -1,10 +1,17 @@
 import { View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
-export default function PickerItem(){
+export default function PickerItem(props){
+
+    let moedasItem = props.moedas.map((item, index) => {
+        return <Picker.Item value = {item.key} key = {index} label = {item.key} />
+    } )
     return(
-       <Picker>
-        <Picker.Item value = 'BTC' key = {0} label = 'BTC' />
+       <Picker
+       selectedValue={props.moedaSelecionada}
+       onValueChange={ (valor) => props.onChange(valor) }
+       >
+        {moedasItem}
        </Picker>
     )
 }
